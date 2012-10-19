5095765213ce989d498e8a40b2e4724c1e37d63c
Pablo Fernandez <fernandezpablo85@gmail.com>
2012-10-19 17:22:33 -0300

    proxy functionality

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 var express = require('express');
+var http = require('http');
+var url = require('url');
 var port = 9000;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('scds.server', 'localhost')
+
 app.get('/', function(req, res){
   res.redirect('/public/index.html');
 });
+
+app.get('/proxy', function(req, res) {
+  var correctParameters = ('url' in req.query && 'method' in req.query);
+  if (correctParameters){
+    proxyRequest(req, res);
+  } else {
+    res.statusCode = 400;
+    res.end(JSON.stringify({'error' : 'must provide both "url" and "method"'}));
+  }
+});
+
+function proxyRequest(req, res) {
+  var proxyTo = url.parse(req.query['url'], true);
+  proxyTo.method = req.query['method'];
+  http.request(proxyTo, function(_res) {
+    _res.pipe(res);
+  }).end();
+}
 
 app.listen(port);
 console.log('express listening in port', port, '...')
