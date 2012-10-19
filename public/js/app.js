7cf73b69e6377b2ab15ff216601b49f550f584ee
Alejandro Bologna <bologna.alejandro@gmail.com>
2012-10-19 20:23:36 -0300

    layout redesign

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   var templateEditor = ace.edit("template-editor");
-  templateEditor.setTheme("ace/theme/twilight");
+  templateEditor.setTheme("ace/theme/chrome");
   templateEditor.getSession().setTabSize(2);
   templateEditor.getSession().setUseSoftTabs(true);
+  templateEditor.getSession().setMode("ace/mode/html");
 
   var url = "/proxy?url=http%3A%2F%2Feat1-app53.corp.linkedin.com%3A8080%2Fscds%2Fdust%2FdevBuild%2Findex%3Ff%3Dtl%2Fapps%26e%3Dtl&method=GET";
   $.get(url, function(apps) {
@@ -16,7 +17,7 @@ $(document).ready(function(){
     $('#search').typeahead({ source: templates, updater: function (item) {
       var scdsUrl = "/proxy?url=" + encodeURIComponent("http://eat1-app53.corp.linkedin.com:8080/scds/dust/devBuild/tl?f=tl/apps" + item.replace(/(\.[^.]+$)/, ""));
       var url = scdsUrl + "&method=GET" ;
-      $.get(url, function(data) { 
+      $.get(url, function(data) {
         templateEditor.setValue(data);
       });
       return item;
@@ -25,8 +26,13 @@ $(document).ready(function(){
 
   var jsonEditor = ace.edit("json-editor");
   jsonEditor.setValue("{json:'sample'}");
-  jsonEditor.setTheme("ace/theme/twilight");
+  jsonEditor.setTheme("ace/theme/chrome");
   jsonEditor.getSession().setMode("ace/mode/javascript");
+
+  var htmlEditor = ace.edit("html-editor");
+  jsonEditor.setValue("");
+  jsonEditor.setTheme("ace/theme/chrome");
+  jsonEditor.getSession().setMode("ace/mode/html");
 
   $('#json-load').click(function(){
     $.get('/proxy?url=' + encodeURIComponent($('#json-url').val()) + '&method=GET', function(json){
